eab6b3774fdb7cb6db430cdaa603a05b34a8464b
noamteyssier <nb.teyssier@gmail.com>
2019-04-05 09:34:04 -0700

    commit data and src

---
+++ controls/ControlDiff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(rc={'figure.figsize':(22, 22), 'lines.linewidth': 5})
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
