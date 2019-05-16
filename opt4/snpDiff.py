0da52c7af118fcb01295477f17787fee504efb83
noamteyssier <nb.teyssier@gmail.com>
2019-05-16 14:09:24 -0700

    added variant calling information and early visualizations

--- opt4/snpDiff.py
+++ opt4/snpDiff.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 import pandas as pd
-import vcf as pyvcf
-import sys
-from scipy.spatial.distance import squareform, pdist
-from statsmodels.formula.api import ols
-
+import vcf
+import sys, os
+# from scipy.spatial.distance import squareform, pdist
+# from statsmodels.formula.api import ols
+#
 import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set(rc={'figure.figsize':(22, 22), 'lines.linewidth': 5})
@@ -19,7 +19,6 @@ def vcf_mat(vcf):
 
     mat = pd.DataFrame(np.vstack(as_mat), columns=vcf.samples).T
     return mat
-
 def sample_dist(mat, plot=True):
     distances = pd.DataFrame(squareform(pdist(mat, 'sqeuclidean')))
     distances.index = distances.columns = vcf.samples
@@ -27,7 +26,6 @@ def sample_dist(mat, plot=True):
         sns.heatmap(distances)
         plt.show()
     return distances
-
 def snp_sum(mat, plot=True):
     mat = mat.T
     mat['snp_sum'] = mat.apply(lambda x : x.sum(), axis = 1)
@@ -35,7 +33,6 @@ def snp_sum(mat, plot=True):
         sns.distplot(mat.snp_sum)
         plt.show()
     return mat
-
 def sampleSingleSnps(mat):
     def single_snps(x):
         nonzero = np.nonzero(x.values)[0]
@@ -50,45 +47,81 @@ def sampleSingleSnps(mat):
         reset_index().\
         rename(columns={'index' : 'sampleName'})
 
-def main():
-    snp_fn = "data/snpDiff.tab"
-    vcf_fn = "data/intersect_replicateVariance.vcf"
-    vcf = pyvcf.Reader(open(vcf_fn, 'r'))
+class SampleVariants:
+    def __init__(self, dir, fn):
+        self.dir = dir
+        self.fn = fn
+        self.sampleName = fn.strip('.vcf').strip('snpcore_')
 
-    # vcf.samples
-    # vcf.infos
+        self.gt_conversion = {'A' : 0, 'C' : 1, 'T' : 2, 'G' : 3}
 
-    mat = vcf_mat(vcf)
-    distances = sample_dist(mat)
-    ssum = snp_sum(mat)
-    singleSnps = sampleSingleSnps(mat)
+        self.open_vcf()
+    def open_vcf(self):
+        """read vcf into dataframe"""
+        sample_df = []
+        header = 0
+        for record in vcf.Reader(open(self.dir + self.fn, 'r')):
+            sample_df.append((self.sampleName, record.CHROM, record.POS, self.gt_conversion[str(record.ALT[0])], record.QUAL, record.INFO['AF1']))
+            header += 1
+            # if header == 1000:
+            #     break
+        self.vcf = pd.DataFrame(sample_df, columns = ['sampleName','chrom', 'pos', 'alt', 'qual', 'af'])
+        self.vcf = self.vcf.set_index(['chrom', 'pos', 'sampleName'])
+        # self.vcf.columns = col_index
+def merge_variants(dir):
+    vcf_fns = [i for i in os.listdir(dir) if '.vcf' in i]
+    variant_calls = []
+    for v in vcf_fns:
+        print(v)
+        sv = SampleVariants(dir, v)
+        variant_calls.append(sv.vcf)
 
-    singleSnps[['density','extraction','digest','swga','rep']] = singleSnps.apply(lambda x : x.sampleName.split('-'), result_type='expand', axis = 1)
-
-    # single snp distribution by density
-    [sns.distplot(singleSnps[singleSnps.density==i].num_singlesnps) for i in ['100', '1000']]
-
-    # categorical differences with p1000
-    sns.catplot(data=singleSnps[singleSnps.density=='1000'],
-        row='extraction', col='swga', kind='violin', inner='stick',
-        x='digest', y ='num_singlesnps', height=8)
-
-    piv_singles = pd.pivot_table(singleSnps, index=['density', 'extraction', 'digest', 'swga'], columns='rep', values='num_singlesnps').\
+    merged_v = pd.concat(variant_calls).\
+        reset_index()
+    pivot_v = pd.pivot_table(merged_v, index = ['chrom', 'pos', 'sampleName'], values=['alt', 'af', 'qual'])
+    pivot_v.to_csv("resources/merged_variants.tab.gz", sep="\t")
+def plot_snp_sample_dist(merged_v):
+    """distribution of number of samples across variants"""
+    ns = merged_v.reset_index()[['chrom', 'pos', 'num_samples']].drop_duplicates()
+    sns.distplot(ns.num_samples, kde=False)
+    plt.show()
+def single_sample_snps(merged_v):
+    """visualization of single_sample_snps by group"""
+    single_snps = merged_v.\
+        groupby(level=2).\
+        num_samples.\
+        apply(lambda x : x[x==1].size).\
         reset_index().\
-        rename(columns={'1':'rep1', '2':'rep2'})
+        rename(columns={'num_samples' : 'num_single_snps'})
 
-    # replicate differences
-    piv_singles['rep_diff'] = np.abs(piv_singles['rep1'] - piv_singles['rep2'])
-    [sns.distplot(piv_singles[(piv_singles.rep_diff <= 200) & (piv_singles.density == i)].rep_diff) for i in ['100', '1000']]
-    [sns.distplot(piv_singles[(piv_singles.density == '1000') & (piv_singles.digest == i)].rep_diff, rug=True) for i in ['M', 'R']]
+    single_snps[['density', 'extraction', 'digest', 'swga', 'rep']] = \
+        single_snps.apply(lambda x : x.sampleName.split('-'), axis = 1, result_type='expand')
+
+    sns.catplot(
+        data=single_snps,
+        x = 'density',
+        y = 'num_single_snps',
+        col='swga',
+        row='extraction',
+        hue='digest',
+        kind='bar',
+        height=10)
+    plt.show()
+    plt.close()
+
+def main():
+    dir = "variant_calls/"
+    # merge_variants(dir)
+    merged_v = pd.read_csv("resources/merged_variants.tab.gz", sep='\t').set_index(['chrom', 'pos', 'sampleName'])
+
+    # apply quality filter on snps
+    merged_v = merged_v[merged_v.qual > 1]
+    merged_v['num_samples'] = merged_v.groupby(level = [0,1]).apply(lambda x : x.shape[0])
+
+    plot_snp_sample_dist(merged_v)
+    single_sample_snps(merged_v)
 
 
-    # ANOVA testing of replicate differences
-    anova_test = piv_singles[piv_singles.rep_diff < 300]
-    ols('rep_diff ~ density + extraction + digest + swga', data= anova_test).fit().summary()
-
-    # ANOVA testing of single_snps 
-    ols('num_singlesnps ~ density + extraction + digest + swga', data=singleSnps).fit().summary()
 
 if __name__ == '__main__':
     main()
