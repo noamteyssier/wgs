9ebda4b7e8f16ab797a912c542c33c2f9039ff12
noamteyssier <nb.teyssier@gmail.com>
2019-05-17 12:04:32 -0700

    add human mapping on same plot

--- optim/src/genomeCov.R
+++ optim/src/genomeCov.R
@@ -41,13 +41,13 @@ plot_percentiles <- function(percentiles_df){
 }
 plot_flagstat <- function(flagstat){
   # plot flagstat statistics by group
-  flagstat$statistic <- factor(flagstat$statistic, levels=c('paired', 'mapped', 'total'))
-  ggplot(flagstat, aes(x = sampleName, y = value/2, fill=digest)) +
+  flagstat$statistic <- factor(flagstat$statistic, levels=c('human', 'mapped', 'total'))
+  ggplot(flagstat %>% filter(statistic != 'paired'), aes(x = sampleName, y = value/2, fill=digest)) +
     geom_bar(stat='identity', position='identity', alpha = 0.5, aes(color=statistic)) +
     facet_wrap(extraction~swga, scales='free_x') +
     theme_classic() +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, size = 10)) +
-    scale_color_brewer(palette='RdBu') +
+    scale_color_brewer(palette='Set2') +
     scale_fill_brewer(palette='Blues') %>%
     return()
 }
@@ -55,7 +55,7 @@ plot_mapping <- function(pc_flagstat){
   # plot percent mapping by group
   ggplot(pc_flagstat, aes(x = as.factor(density), y = pc_mapped, fill=digest, color=as.factor(rep))) +
     geom_bar(stat='identity', position='dodge') +
-    facet_wrap(swga~extraction) +
+    facet_wrap(extraction~swga) +
     theme_classic() +
     xlab("Parasite Density") +
     ylab("Percent of Reads Mapping to Human") +
@@ -72,15 +72,19 @@ flagstat <- read_tsv("../data/opt4/flagstat.tab")
 percentiles_df <- melt_pc_df(percentiles_df)
 flagstat <- melt_flagstat(flagstat)
 human_flagstat <- melt_flagstat(human_flagstat)
-human_mapping <- percent_mapping(human_flagstat)
+pc_human_mapping <- percent_mapping(human_flagstat)
+
+human_mapped <- human_flagstat %>%
+  filter(statistic == 'mapping') %>%
+  mutate(statistic = 'human')
+flagstat <- rbind(flagstat, human_mapped)
+
 
 # plot
 percentile_cov_plot <- percentiles_df %>% plot_percentiles()
 flagstat_plot <- flagstat %>% plot_flagstat()
-human_flagstat_plot <- human_flagstat %>% plot_flagstat()
-human_mapping_plot <- human_mapping %>% plot_mapping()
+human_mapping_plot <- pc_human_mapping %>% plot_mapping()
 
 ggsave("../plots/percentile_coverage.png", percentile_cov_plot, width = 8, height = 6)
 ggsave("../plots/pf_flagstat.png", flagstat_plot, width = 8, height = 6)
-ggsave("../plots/hum_flagstat.png", human_flagstat_plot, width = 8, height = 6)
 ggsave("../plots/human_mapping.png", human_mapping_plot, width = 8, height = 6)
