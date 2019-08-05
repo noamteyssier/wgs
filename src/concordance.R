93b468b405261472d6049942bfc625ed0859ef86
noamteyssier <nb.teyssier@gmail.com>
2019-08-05 13:25:01 -0700

    update concordance to reflect high quality, biallelic, snps with 5 minDP

--- src/concordance.R
+++ src/concordance.R
@@ -1,40 +1,54 @@
 library(tidyverse)
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
-errors <- read_tsv("../data/complete_set/snpConcordance.tab")
+errors <- read_tsv("../data/complete_set/concordance.tab")
 
 single_strains <- c('U51', 'fcr3')
-single_strains_all <- unique(errors$sample_i) %>% tail(7)
 
 similarity <- errors %>%
-  filter(sample_i %in% single_strains) %>%
-  filter(sample_j %!in% single_strains_all) %>%
-  group_by(sample_j) %>%
-  mutate(errorRate = 1 - errorRate) %>%
-  top_n(1, errorRate)
+  filter(
+    (s1 %in% single_strains) & (s2 %!in% single_strains)) %>%
+  group_by(s2) %>%
+  mutate(concordance = 1 - pc_error) %>%
+  top_n(1, concordance)
 
 grouped_sims <- similarity %>%
   extract(
-    sample_j,
+    s2,
     into=c('density', 'extraction', 'digest', 'swga', 'rep'),
     regex="([[:alnum:]]+)-([[:graph:]]+)-([[:graph:]])-([[:graph:]]+)-([[:alnum:]])"
   ) %>%
   group_by(density, extraction, digest, swga) %>%
-  summarise(m_err = mean(errorRate)) %>%
+  summarise(m_concordance = mean(concordance)) %>%
   ungroup() %>%
   mutate(density = paste0(density, ' p/uL')) %>%
-  add_row(density = '10 p/uL', extraction = 'Che', digest='R', swga='Sof', m_err=0.001)
+  add_row(density = '10 p/uL', extraction = 'Che', digest='R', swga='Sof', m_concordance=0.001)
 
+hex = c(
+    '#e45c26',
+    '#00B26e',
+    '#efa89a',
+    '#88D5A9',
+    '#ffe6de',
+    '#bfead0'
+  ) %>% rev()
 
-concordance <- ggplot(grouped_sims, aes(x = interaction(swga,extraction), y = m_err * 100, fill = digest)) +
-  geom_bar(stat='identity', position='dodge') +
-  theme_minimal() +
-  facet_wrap(~density) +
-  theme(axis.text.x = element_text(angle=60, hjust=1)) +
-  scale_fill_brewer(palette='Paired') +
+grouped_sims$digest <- factor(grouped_sims$digest, levels=c('R', 'M'))
+grouped_sims$swga <- factor(grouped_sims$swga, levels=c('Sof', 'Sang'))
+
+concordance_plt <- ggplot(grouped_sims,
+    aes(
+      x = interaction(digest, interaction(swga,extraction)),
+      y = m_concordance,
+      fill = extraction)) +
+  geom_bar(stat='identity', position='dodge', size=.1, color='black', width=1) +
+  theme_bw() +
+  facet_grid(~density, scales='free_x', space='free') +
+  theme(axis.text.x = element_text(angle=45, hjust=.95, vjust=.95)) +
+  scale_fill_manual(values=hex %>% tail(2)) +
   xlab("sWGA and Extraction Condition") +
   ylab("SNP Concordance to Reference (VS1 / fcr3)") +
-  scale_y_continuous(breaks = seq(0, 100, 10))
+  scale_y_continuous(breaks = seq(0, 1, .1), expand=c(0.001,0.01), limits=c(0,1))
 
-ggsave("../plots/concordance.pdf", concordance, width = 8, height = 6)
-ggsave("../plots/concordance.png", concordance, width = 8, height = 6)
+ggsave("../plots/concordance.pdf", concordance_plt, width = 8, height = 6)
+ggsave("../plots/concordance.png", concordance_plt, width = 8, height = 6)
